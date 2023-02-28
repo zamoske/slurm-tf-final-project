ffe3c2e1a157bbc27a6abe5e68303ad43dc2d45a
Artem <113437006+zamoske@users.noreply.github.com>
2023-02-28 16:38:26 +0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,6 +8,6 @@ terraform {
   required_version = ">= 0.13"
 }
 provider "yandex" {
-  token     = "y0_AgAAAAANPTr-AATuwQAAAADZYfk2i09LDVJvRsO39UgPLX8Ff6Iz5r8"
-  cloud_id  = "b1gncohuennqad075k5v"
+  token     = $YC_TOKEN_ID
+  cloud_id  = $YC_CLOUD_ID
 }
